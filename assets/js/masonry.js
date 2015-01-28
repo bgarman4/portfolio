aa246246f31267ff506da654945409b5294ec1ce
Brandon Garman <bdg1@clevelandmetroparks.com>
2015-01-27 21:21:05 -0500

    initial commit

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,7 +1,28 @@
-var container = document.querySelector('#work-examples');
+/*var container = document.querySelector('#work-examples');
 var msnry = new Masonry( container, {
   // options...
   itemSelector: '.item',
   columnWidth: 10,
   gutter: 4
+});*/
+
+docReady( function() {
+
+  var container = document.querySelector('.work-examples');
+  var msnry = new Masonry( container, {
+    columnWidth: 10,
+    gutter: 4
+  });
+
+  eventie.bind( container, 'click', function( event ) {
+    // don't proceed if item was not clicked on
+    if ( !classie.has( event.target, 'thumbnail' ) ) {
+      return;
+    }
+    // change size of item via class
+    classie.toggle( event.target, 'w3' );
+    // trigger layout
+    msnry.layout();
+  });
+
 });
